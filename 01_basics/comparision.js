3fc31697945b9ffb5f13891c049a962786139067
Aayush Deshmukh <aayushworkspace814@gmail.com>
2025-01-23 08:04:59 +0000

    comparision of diff datatypes

---
+++ 01_basics/comparision.js
@@ -0,0 +1,18 @@
+//console.log(2 > 1)
+// console.log(2 >= 1)
+// console.log(2 < 1)
+// console.log(2 == 1)
+//console.log(2 != 1)
+
+// console.log("2" > 1)
+// console.log("02" >1)    //diffrent datatypes ka conversion is not good/reliable
+// console.log("2" == 2)
+
+console.log(null > 0)
+console.log(null == 0)    //equality check (==) and comparisons (> < >=) work diffrently 
+console.log(null >= 0)
+
+console.log(undefined == 0)
+console.log(undefined == 0)
+console.log(undefined >= 0)
+
