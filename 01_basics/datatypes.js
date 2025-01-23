1fd8b159a5f16f618215347b7356af9be001b3ad
Aayush Deshmukh <aayushworkspace814@gmail.com>
2025-01-23 06:15:06 +0000

    Conversion of datatypes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,19 @@
+
+const id = 1
+let name = "Aayush"
+let age = 21
+let isLoggedIn = false
+
+// it is generally fine to omit semicolons in JavaScript due to Automatic Semicolon Insertion (ASI)
+//In JavaScript, variables are defined using the keywords const' and let' (var' is generally avoided), 
+//and the data type of the variable is determined automatically based on the value you assign to it.
+
+
+
+/*number = 2 ^ 53
+bigint = for large numbers
+string = "assembly of characters"
+boolean = true or false
+null = stand alone value (type = object)
+undefined = kindda place holder for a value which is not defined yet (type = undefined)
+symbol = uniquness */
