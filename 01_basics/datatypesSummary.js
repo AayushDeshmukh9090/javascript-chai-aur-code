fafb66bea2378b97f4fec1812c144ff53d24dc58
Aayush Deshmukh <aayushworkspace814@gmail.com>
2025-01-25 13:15:55 +0000

    Datatypes Summary

--- 01_basics/datatypesSummary.js
+++ 01_basics/datatypesSummary.js
@@ -1,6 +1,22 @@
-//Premitive
+//Primitive => fixed size and data stored directly in stack
+// 7 types : String, Number, Boolean, null. undefined, Symbol, BigInt
 
+//Non-Primitive: custom size and hold a reference to a memory location where the actual data is stored (heap)
+//Array
+const heros = ["Iron man", "Captain America", "Super Man"]
+console.log(heros[2])
+const IDs = [1, 2, 3]
+console.log(IDs[2])
 
-//Non-Premitive
+//objects
+let myObj = {
+   name : "Aayush",
+   age : 21,
+}
+
+//Functions
+const myFunction = function(){
+    console.log("hello World")
+}
 
 //to master JS => Master objects and Browser events  
