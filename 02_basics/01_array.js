152043d990a2afca76a2de8d13008f8f550f99b0
Aayush Deshmukh <aayushworkspace814@gmail.com>
2025-02-04 09:14:18 +0000

    02_basics

---
+++ 02_basics/01_array.js
@@ -0,0 +1,5 @@
+//array
+//can contain a mix of different data types. index starts form 0 to infinite(size is dynamic)
+const myArr1 = [1, 2, 3, 4, "Aayush", 2.4, true];
+const temp1 = [11, 12 , 13, 14 , 15]
+const temp2 = temp1
