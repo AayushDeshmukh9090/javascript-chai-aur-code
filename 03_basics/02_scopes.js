d765a3272b17cdb02bbce58cc0c81a0067677047
Aayush Deshmukh <aayushworkspace814@gmail.com>
2025-02-11 03:50:57 +0000

    scopes

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -1,4 +1,4 @@
-  function first(){
+  if(true){
 
     const a= 10
     let b = 20
@@ -6,9 +6,33 @@
 
   }
 
-  console.log(a)
-  console.log(b)
-  console.log(c)
+  //console.log(a)
+  //console.log(b)
+   console.log(c)
+
+  function one(){
+    const userName = "Aayush"
+     function two(){
+       if(userName == "Aayush"){
+        console.log("variables of one are accessible!")
+       }
+       const location = "Jaipur"
+
+     }
+     //console.log(location)
+     two()
+  }
+  
+  console.log(addOne(5))
+  function addOne(num){ 
+    return num+1
+  }
+
+  //console.log(addTwo(5))        //function ko with a variable declare kiya ho to delaration ke baad hi use kar sakte hai
+  const addTwo = function(num){     
+    return num + 2
+  }
+  console.log(addTwo(5))
 
 
 
