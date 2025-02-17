363b2b7e0aa97bc93a61b6fa7a0a6f82422b0d8e
Aayush Deshmukh <aayushworkspace814@gmail.com>
2025-02-17 17:10:43 +0000

    controlFlow1

---
+++ 04_control_flow/one.js
@@ -0,0 +1,8 @@
+if(2 == "2"){
+    console.log("executed!")   //datatypes same hone se matlab nahi
+}
+
+if(2==="2"){
+    console.log("executed again!")         //datatypes bhi same ho
+}
+else{console.log("did not execute!")}
